8ad4764cfb1d5a4cb1abd806443fffa80aaa1110
Daniel Moros <danielito.moros03@gmail.com>
2025-05-10 11:13:33 -0400

    Update intento.cpp

--- intento.cpp
+++ intento.cpp
@@ -1,55 +1,66 @@
 #include<iostream>
 #include<string>
 #include<fstream>
-
 using namespace std;
 
 class Numor{
-
     public:
-
-    int id;
-    string nombre;
-    string tipo;
-    int dmg;
-    int life;
+        int id;
+        string nombre;
+        string tipo;
+        int dmg;
+        int life;
 }; 
 
-
-int main(){
-
-    int N, pisos;
-
-    ifstream DB("NumorisDB.in"); 
-
-    if(!DB.is_open()){
-        cout << "No encentro el archivo Papi. " << endl;
+// Función para leer el archivo NumorisDB.in
+int leerNumoris(const string& filename, Numor numoris[]) {
+    ifstream DB(filename);
+    if (!DB.is_open()) {
+        cout << "Archivo no encontrado." << endl;
         return -1;
-    } 
-    
+    }
+
+    int N;
     DB >> N; DB.ignore();
 
-    Numor numoris[N], solucion[N]; // Para guardar los datos de los numoris, y de los equipos que llegan al final de la torre con vida
-    
-
-    for(int i = 0; i < N; i++){
-        DB >> numoris[i].id >> numoris[i].nombre >> numoris[i].tipo >> numoris[i].dmg >> numoris[i].life; 
+    for (int i = 0; i < N; i++) {
+        DB >> numoris[i].id >> numoris[i].nombre >> numoris[i].tipo >> numoris[i].dmg >> numoris[i].life;
     }
 
     DB.close();
-    int torreNum;
-    cin >> torreNum;
-    char archivo2[15];
+    return N; 
+}
 
-    sprintf(archivo2, "Torre%d.in", torreNum); //Hace lo que te dije en Ws
-    ifstream torre(archivo2);
-
+// Función para leer el archivo TorreX.in
+void leerTorre(string& filename, int& pisos, int numoris[]) {
+    /*
+        int torreNum;
+        cin >> torreNum;
+        char archivo2[15];
+        sprintf(archivo2, "Torre%d.in", torreNum); 
+    */
+    filename += to_string(pisos); // Agrega el número de torre al nombre del archivo
+    filename += ".in"; // Agrega la extensión .in
+    
+    ifstream torre(filename);
     if(!torre.is_open()){
-        cout << "No lo encuentro, mi amor";
+        cout << "Archivo no encontrado." << endl;
         return -1;
     }
 
     torre >> pisos; torre.ignore(); //Guarda la cantidad de pisos
-    
+    torre.close();
+}
+
+int main(){
+    int N, pisos, X; // En duda 
+
+    Numor *numoris;
+    int NumorisDB = leerNumoris("NumorisDB.in", numoris);
+    numoris = new Numor[NumorisDB];
+
+    cin >> X; // X en el archivo TorreX.in
+    leerTorre("Torre", X, numoris);
+
     return 0;
 }
