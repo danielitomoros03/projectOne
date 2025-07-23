a4e140b0312f4ca2a1883f703e0a724a65ccacbf
Daniel Moros <danielito.moros03@gmail.com>
2025-07-23 18:09:45 -0400

    Tarea lista

--- AyED/Proyecto 2/tarea2.cpp
+++ AyED/Proyecto 2/tarea2.cpp
@@ -581,7 +581,11 @@ class BST{
 int main(){
     int N[] = {5, 10, 15, 20};
     BST arboles[4];
+<<<<<<< Updated upstream
     for (int i = 0; i < 4; i++) { // Poner que es hasta 4 árboles
+=======
+    for (int i = 0; i < 4; i++) {
+>>>>>>> Stashed changes
         int pasos = 1;
         while (pasos < pow(2, N[i])) pasos *= 2; 
         for (int k = pasos/2; k > 0; k /= 2) {
